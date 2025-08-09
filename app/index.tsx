6e19b0a5686056e3c6f42219afcf7c239134b27f
markm <markmutisya72@gmail.com>
2025-08-09 10:38:59 -0700

    Configuring Tailwind CSS and Update on Project

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-xl font-bold text-blue-500">
-                Welcome to Nativewind!
+                Welcome too Nativewind!
             </Text>
         </View>
     );
